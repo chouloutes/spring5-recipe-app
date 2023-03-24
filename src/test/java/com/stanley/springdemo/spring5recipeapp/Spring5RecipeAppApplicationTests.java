c6c83c9ad7c462fe636036981ad80b7dc7902ddb
Stanley Chouloute <chouloute.stanley@heb.com>
2023-03-24 15:01:13 -0500

    first commit

---
+++ src/test/java/com/stanley/springdemo/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stanley.springdemo.spring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
